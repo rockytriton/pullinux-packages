268836a1a68bff3d037c78891cfff56e77abcf5b
rockytriton <rockytriton@yahoo.com>
2020-12-13 14:29:11 -0600

    fix zstd

--- z/zstd/build.sh
+++ z/zstd/build.sh
@@ -3,7 +3,7 @@ make
 mkdir -p $P/usr
 mkdir -p $P/lib
 
-make DESTDIR=$P prefix=$P/usr install
+make DESTDIR=$P prefix=/usr install
 
 rm -v $P/usr/lib/libzstd.a
 mv -v $P/usr/lib/libzstd.so.* $P/lib
