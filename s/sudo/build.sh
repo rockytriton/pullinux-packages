c966bcb846ce802d332aecacfc9cf1be7625d61b
rockytriton <rockytriton@yahoo.com>
2020-12-14 23:34:37 -0600

    update sudo

--- s/sudo/build.sh
+++ s/sudo/build.sh
@@ -11,3 +11,26 @@ make DESTDIR=$P install
 
 ln -sfv libsudo_util.so.0.0.0 $P/usr/lib/sudo/libsudo_util.so.0
 
+mkdir -p $P/etc/sudoers.d
+
+cat > $P/etc/sudoers.d/sudo << "EOF"
+Defaults secure_path="/usr/bin:/bin:/usr/sbin:/sbin"
+%wheel ALL=(ALL) ALL
+EOF
+
+cat > /etc/pam.d/sudo << "EOF"
+# include the default auth settings
+auth      include     system-auth
+
+# include the default account settings
+account   include     system-account
+
+# Set default environment variables for the service user
+session   required    pam_env.so
+
+# include system session defaults
+session   include     system-session
+EOF
+
+chmod 644 /etc/pam.d/sudo
+
