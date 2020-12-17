7ae95f9563713acd64f4510808bc25fe1ab9f741
rockytriton <rockytriton@yahoo.com>
2020-12-17 09:56:43 -0600

    remove graphite2 docs

--- g/graphite2/build.sh
+++ g/graphite2/build.sh
@@ -6,14 +6,5 @@ cd    build &&
 cmake -DCMAKE_INSTALL_PREFIX=/usr .. &&
 make
 
-make docs
-
 make DESTDIR=$P install
-
-install -v -d -m755 $P/usr/share/doc/graphite2-1.3.14 &&
-
-cp      -v -f    doc/{GTF,manual}.html \
-                    $P/usr/share/doc/graphite2-1.3.14 &&
-cp      -v -f    doc/{GTF,manual}.pdf \
-                    $P/usr/share/doc/graphite2-1.3.14
 
