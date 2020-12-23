6c396ef9b53aa30c739b195bede344339ffc5a4e
rockytriton <rockytriton@yahoo.com>
2020-12-23 14:03:25 -0600

    fix pyc

--- p/pycairo/build.sh
+++ p/pycairo/build.sh
@@ -1,6 +1,4 @@
 python3 setup.py build
 
-python3 setup.py install --optimize=1 --root=$P   &&
-python3 setup.py install_pycairo_header --root=$P &&
-python3 setup.py install_pkgconfig --root=$P
+python3 setup.py install --optimize=1 --root=$P 
 
