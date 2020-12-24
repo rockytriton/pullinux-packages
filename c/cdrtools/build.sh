3d4b4d0dc8e7c6e81740f19f69b44fe52676e3a8
rockytriton <rockytriton@yahoo.com>
2020-12-23 21:52:02 -0600

    fix cdrt

--- c/cdrtools/build.sh
+++ c/cdrtools/build.sh
@@ -2,8 +2,5 @@ export GMAKE_NOWARN=true &&
 make -j1 INS_BASE=/usr DEFINSUSR=root DEFINSGRP=root
 
 export GMAKE_NOWARN=true &&
-make DESTDIR=$P INS_BASE=/usr MANSUFF_LIB=3cdr DEFINSUSR=root DEFINSGRP=root install &&
-install -v -m755 -d $P/usr/share/doc/cdrtools-3.02a09 &&
-install -v -m644 README* ABOUT doc/*.ps \
-                    $P/usr/share/doc/cdrtools-3.02a09
+make DESTDIR=$P INS_BASE=/usr MANSUFF_LIB=3cdr DEFINSUSR=root DEFINSGRP=root install
 
